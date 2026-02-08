e816438261a63090f5c41ab7fe8a84d3039adb87
sayand babu <sayand23bcs180@iiitkottayam.ac.in>
2026-02-08 13:53:28 +0530

    started working on admin pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,13 @@ import Loading from './pages/Loading';
 import Navbar from './components/Navbar';
 import Chatbox from './components/Chatbox';
 import { Toaster } from 'react-hot-toast';
+import Layout from '../src/pages/admin/Layout';
+import Dashboard from '../src/pages/admin/Dashboard';
+import CredentialVerify from '../src/pages/admin/CredentialVerify';
+import CredentialChange from '../src/pages/admin/CredentialChange';
+import AllListings from '../src/pages/admin/AllListings';
+import Transactions from '../src/pages/admin/Transactions';
+import Withdrawal from '../src/pages/admin/Withdrawal';
 
 const App = () => {
   const { pathname } = useLocation();
@@ -28,6 +35,15 @@ const App = () => {
         <Route path="/messages" element={<Messages />} />
         <Route path="/my-orders" element={<MyOrders />} />
         <Route path="/loading" element={<Loading />} />
+
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="verify-credentials" element={<CredentialVerify />} />
+          <Route path="change-credentials" element={<CredentialChange />} />
+          <Route path="list-listings" element={<AllListings />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="withdrawal" element={<Withdrawal />} />
+        </Route>
       </Routes>
       <Chatbox />
     </div>
