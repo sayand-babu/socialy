5f30dc2ed19c2c57cf5917625408e5117d76ea2e
sayand babu <sayand23bcs180@iiitkottayam.ac.in>
2026-02-28 13:21:35 +0530

    fixed import issue

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import prisma from "../config/prisma";
+import prisma from "../config/prisma.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "socjal-media-marketplace" });
 
